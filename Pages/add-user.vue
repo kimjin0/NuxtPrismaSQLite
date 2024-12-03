d868b5130e750c4715749a78c03fb2c9b169d474
kimjin0 <you@example.com>
2024-12-03 21:31:23 +0900

    temple 작업중..

---
+++ Pages/add-user.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Add User</h1>
+      <input v-model="name" placeholder="Name" />
+      <input v-model="email" placeholder="Email" />
+      <button @click="addUser">Add User</button>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('');
+const email = ref('');
+const message = ref('');
+
+const addUser = async () => {
+  const response = await $fetch('/api/users/add', {
+    method: 'POST',
+    body: { name: name.value, email: email.value },
+  });
+
+  message.value = response.message;
+};
+</script>
+
+
